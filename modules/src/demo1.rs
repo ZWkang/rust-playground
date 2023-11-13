29fe9185d0ec24f8e1943673453b638699b25205
zwkang <kang95630@gmail.com]>
2023-11-13 20:21:06 +0800

    feat: 增加模块的使用

---
+++ modules/src/demo1.rs
@@ -0,0 +1,7 @@
+pub mod front_of_house {
+  pub mod hosting {
+      pub fn add(left: usize, right: usize) -> usize {
+        left + right
+      }
+  }
+}
---
+++ modules/src/main.rs
@@ -0,0 +1,9 @@
+mod demo1;
+use demo1::front_of_house::hosting::add;
+
+
+pub fn main() {
+    println!("Hello, world!");
+
+    println!("demo1 value: {}", add(3, 4));
+}
